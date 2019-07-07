600340032bff48c6c7bc3afa85aa6d41c27554f5
rishabhenu <Rishabh.Sharma@openlane.com>
2019-07-07 12:54:37 +0530

    renamed class

--- src/permutationANDcombination/P003PermutationAndCombination.java
+++ src/permutationANDcombination/P003PermutationAndCombination.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class PermutationAndCombination {
+public class P003PermutationAndCombination {
 
 	public static void main(String...strings){
 		int[] array  = {1,2,3};
